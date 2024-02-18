d487d21c47faae6cbe4390edee6835dcfcf37be0
rubenangber <rubigrowl@gmail.com>
2024-02-18 14:48:16 +0100

    Creador de contemido

---
+++ Creador de contenido/README.md
@@ -0,0 +1,21 @@
+# EJERCICIO INTRODUCCIÓN POO JAVA
+Un influencer quiere analizar los posibles ingresos de su canal de Youtube realizando un programa de consola en JAVA. El programa deberá solicitar al inicio del programa la siguiente información:
+- Nombre del canal
+- Número de total vídeos subidos
+
+Por cada video, se le solicitará por consola:
+- Título del vídeo (String)
+- Duración del video en segundos (long)
+- Visualizaciones hasta la fecha (long)
+- Id video URL: https://www.youtube.com/watch?v=Sagg08DrO5U (String)
+
+En el programa se establecerá un CPM (Costo por Mil impresiones) actual de Youtube de forma fija e inalterable. Suponemos un CPM de 1.7 € (1.7€ por 1000 visualizaciones) Tras esto se le presentará al usuario una tabla similar a la siguiente:
+
+| Título | Visualizaciones | Duración | URL | Ingresos |
+|----------|----------|----------|----------|----------|
+| Video 1 | 1234 | 20921 | 673&vdyqw | 2,1 |
+| Video 2 | 1000 | 2231 | y3f2g8dd12 | 1,7 |
+|Suponemos más videos ... |<!-- -->  | <!-- -->  | <!-- --> | <!-- --> |
+| Video N | 1000 | 22321 | y421u8d12 | 1,7 |
+| Ingresos totales del canal | <!-- -->  | <!-- -->  | <!-- -->  | 5,5 |
+| Media de ingresos por video | <!-- -->  | <!-- -->  | <!-- -->  | 1,83 |
---
+++ Creador de contenido/src/Video.java
@@ -0,0 +1,57 @@
+public class Video {
+    public String titulo;
+    public long visualizaciones;
+    public long segundos;
+    public String URL;
+    public float ingresos;
+    
+    public Video(String titulo, long visualizaciones, long segundos, String URL) {
+        this.titulo = titulo;
+        this.visualizaciones = visualizaciones;
+        this.segundos = segundos;
+        this.URL = URL;
+        this.ingresos = visualizaciones * 0.0017f;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public long getVisualizaciones() {
+        return visualizaciones;
+    }
+
+    public void setVisualizaciones(long visualizaciones) {
+        this.visualizaciones = visualizaciones;
+    }
+
+    public long getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(long segundos) {
+        this.segundos = segundos;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public float getIngresos() {
+        return ingresos;
+    }
+
+    @Override
+    public String toString() {
+        String retorno = String.format("|%-20s|%-20d|%-20d|%-20s|%-20.2f|", this.titulo, this.visualizaciones, this.segundos, this.URL, this.ingresos);
+        return retorno;
+    }
+}
